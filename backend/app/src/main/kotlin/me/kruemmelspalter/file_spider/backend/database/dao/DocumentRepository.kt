b87c0cad7a2587d12e90ccd6acc6837757e4881e
Kruemmelspalter <kruemmelspalter@duck.com>
2022-10-11 20:33:25 +0200

    Filter/Search Documents (#6)
    * filter documents (positive filter)
    
    * add negative filters
    
    * formatting

--- backend/app/src/main/kotlin/me/kruemmelspalter/file_spider/backend/database/dao/DocumentRepository.kt
+++ backend/app/src/main/kotlin/me/kruemmelspalter/file_spider/backend/database/dao/DocumentRepository.kt
@@ -1,10 +1,12 @@
 package me.kruemmelspalter.file_spider.backend.database.dao
 
 import me.kruemmelspalter.file_spider.backend.database.model.Document
-import me.kruemmelspalter.file_spider.backend.services.DocumentMeta
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 import java.util.Optional
 import java.util.UUID
 
@@ -14,19 +16,12 @@ class DocumentRepository : DocumentDao {
     @Autowired
     val jdbcTemplate: JdbcTemplate? = null
 
+    @Autowired
+    val namedParameterJdbcOperations: NamedParameterJdbcOperations? = null
+
     override fun getDocument(id: UUID): Optional<Document> {
         val documents = jdbcTemplate!!.query(
-            "select * from Document where id = ?",
-            { rs, _ ->
-                Document(
-                    UUID.fromString(rs.getString(1)),
-                    rs.getString(2),
-                    rs.getTimestamp(3),
-                    rs.getString(4),
-                    rs.getString(5),
-                    rs.getString(6)
-                )
-            }, id.toString()
+            "select * from Document where id = ?", { rs, _ -> documentFromResultSet(rs) }, id.toString()
         )
         if (documents.size != 1) return Optional.empty()
         return Optional.of(documents[0])
@@ -34,10 +29,7 @@ class DocumentRepository : DocumentDao {
 
     override fun getTags(id: UUID): List<String> {
         return jdbcTemplate!!.query(
-            "select tag from Tag where document = ?",
-            { rs, _ ->
-                rs.getString(1)
-            }, id.toString()
+            "select tag from Tag where document = ?", { rs, _ -> rs.getString(1) }, id.toString()
         )
     }
 
@@ -49,7 +41,28 @@ class DocumentRepository : DocumentDao {
         TODO("Not yet implemented")
     }
 
-    override fun filterDocuments(filter: String): List<DocumentMeta> {
-        TODO("Not yet implemented")
+    override fun filterDocuments(posFilter: List<String>, negFilter: List<String>): List<Document> {
+        if (posFilter.isEmpty()) return ArrayList()
+
+        return namedParameterJdbcOperations!!.query(
+            "select Document.* from Document left join (select document, count(tag) as tagCount from Tag where " +
+                "tag in (:posTags) group by document) as postags on postags.document = Document.id " +
+                (
+                    if (negFilter.isEmpty()) "join (select NULL as tagCount) as negtags" else
+                        "left join (select document, count(tag) as tagCount from Tag " +
+                            "where tag in (:negTags) group by document) as negtags on negtags.document = Document.id"
+                    ) +
+                " where postags.tagCount = :ptCount and negtags.tagCount IS NULL",
+            MapSqlParameterSource(mapOf("posTags" to posFilter, "negTags" to negFilter, "ptCount" to posFilter.size))
+        ) { rs, _ -> documentFromResultSet(rs) }
     }
+
+    private fun documentFromResultSet(rs: ResultSet) = Document(
+        UUID.fromString(rs.getString(1)),
+        rs.getString(2),
+        rs.getTimestamp(3),
+        rs.getString(4),
+        rs.getString(5),
+        rs.getString(6)
+    )
 }
--- backend/app/src/main/kotlin/me/kruemmelspalter/file_spider/backend/services/DocumentService.kt
+++ backend/app/src/main/kotlin/me/kruemmelspalter/file_spider/backend/services/DocumentService.kt
@@ -2,6 +2,7 @@ package me.kruemmelspalter.file_spider.backend.services
 
 import me.kruemmelspalter.file_spider.backend.FileSystemService
 import me.kruemmelspalter.file_spider.backend.database.dao.DocumentRepository
+import me.kruemmelspalter.file_spider.backend.database.model.Document
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.sql.Timestamp
@@ -18,18 +19,21 @@ class DocumentService {
 
     fun getDocumentMeta(id: UUID): Optional<DocumentMeta> {
         val document = documentRepository!!.getDocument(id)
-        return if (document.isEmpty) Optional.empty()
-        else {
-            val attributes = fsService!!.getFileAttributesFromID(id)
-            Optional.of(
-                DocumentMeta(
-                    document.get(),
-                    Timestamp(attributes.creationTime().toMillis()),
-                    Timestamp(attributes.lastModifiedTime().toMillis()),
-                    Timestamp(attributes.lastAccessTime().toMillis()),
-                    documentRepository!!.getTags(id)
-                )
-            )
-        }
+        return if (document.isEmpty) Optional.empty() else Optional.of(documentToMeta(document.get()))
+    }
+
+    private fun documentToMeta(document: Document): DocumentMeta {
+        val attributes = fsService!!.getFileAttributesFromID(document.id)
+        return DocumentMeta(
+            document,
+            Timestamp(attributes.creationTime().toMillis()),
+            Timestamp(attributes.lastModifiedTime().toMillis()),
+            Timestamp(attributes.lastAccessTime().toMillis()),
+            documentRepository!!.getTags(document.id)
+        )
+    }
+
+    fun filterDocuments(posFilter: List<String>, negFilter: List<String>): List<DocumentMeta> {
+        return documentRepository!!.filterDocuments(posFilter, negFilter).map { documentToMeta(it) }
     }
 }

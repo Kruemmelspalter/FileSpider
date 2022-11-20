2c48265c74ddd8277e2bb59291fda66b9fa817c5
Kruemmelspalter <kruemmelspalter@duck.com>
2022-11-20 19:22:03 +0100

    display tags on search results

--- frontend/pages/_id.vue
+++ frontend/pages/_id.vue
@@ -1,24 +1,34 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer app class="px-3">
+    <v-navigation-drawer app>
       <v-form
         ref="form"
         @submit.prevent="commitSearch"
       >
         <v-text-field
           v-model="search"
+          class="mx-3 mt-5"
           label="Search"
           prepend-icon="mdi-file-search"
           @input="onSearchChange"
           @click:prepend="commitSearch"
         />
-        <v-banner v-show="showSearchError" color="error" icon="mdi-alert">
+        <v-alert v-if="showSearchError" color="error" icon="mdi-alert">
           Search Error
-        </v-banner>
+        </v-alert>
       </v-form>
       <v-list>
         <v-list-item v-for="r in searchResults" :key="r.id" :to="r.id" link nuxt>
-          {{ r.title }}
+          <v-list-item-content>
+            <v-list-item-title>{{ r.title }}</v-list-item-title>
+            <v-list-item-subtitle>
+              <v-chip-group>
+                <v-chip v-for="t in r.tags" :key="t" small @click="search = t">
+                  {{ t }}
+                </v-chip>
+              </v-chip-group>
+            </v-list-item-subtitle>
+          </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -55,17 +65,17 @@
         :data="`${apiSource}/document/${documentID}/rendered`"
         @load="iframeState = 1"
       >
-        <v-banner color="error" icon="mdi-alert">Error loading document</v-banner>
+        <v-alert color="error" icon="mdi-alert">Error loading document</v-alert>
       </object>
-      <v-banner v-if="iframeState === 0" color="secondary">
+      <v-alert v-if="iframeState === 0" color="secondary">
         <v-icon class="turning">
           mdi-loading
         </v-icon>
         Loading...
-      </v-banner>
-      <v-banner v-if="iframeState===2" color="error" icon="mdi-alert">
+      </v-alert>
+      <v-alert v-if="iframeState===2" color="error" icon="mdi-alert">
         Error loading document (timeout)
-      </v-banner>
+      </v-alert>
     </v-main>
   </v-app>
 </template>
@@ -77,13 +87,13 @@ export default {
   layout: 'blank',
   data () {
     return {
-      iframeState: 0, // 0: loading, 1: loaded
-      darkMode: localStorage.getItem('lightMode') === null,
+      iframeState: 0, // 0: loading, 1: loaded, 2: error
+      darkMode: true,
       search: localStorage.getItem('searchTerm') || '',
       time: null,
       interval: null,
       searchTimeout: null,
-      searchResults: [],
+      searchResults: JSON.parse(localStorage.getItem('searchResults') || '[]'),
       showSearchError: false,
       documentInvert: true,
       apiSource: 'http://172.31.69.3',
@@ -119,6 +129,7 @@ export default {
     clearInterval(this.interval)
 
     localStorage.setItem('searchTerm', this.search)
+    localStorage.setItem('searchResults', JSON.stringify(this.searchResults))
   },
   methods: {
     reload () {

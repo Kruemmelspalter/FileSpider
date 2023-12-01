db426e5c77283a3d5767ab940b27007a41745918
Kruemmelspalter <kruemmelspalter@duck.com>
2023-11-30 16:04:56 -0800

    more stupidness

--- src-tauri/src/document/mod.rs
+++ src-tauri/src/document/mod.rs
@@ -334,7 +334,7 @@ pub async fn show_render_in_explorer(
 
         let mut encoded = render.0
             .as_bytes()
-            .into_iter()
+            .iter()
             .chain([0u8])
             .collect::<Vec<u8>>();
 
